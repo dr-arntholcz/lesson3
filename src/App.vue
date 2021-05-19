efd27764c15e732d6e307ac3543b40f678a2cbb8
Stanislav <dr.arntholcz@mail.ru>
2021-05-19 11:44:45 +0700

    esgdfhfj

---
+++ src/App.vue
@@ -0,0 +1,124 @@
+<template>
+  <div id="app">
+    <div :class="[$style.center]">
+      <header :class="[$style.header]">My personal costs</header>
+      <main>
+        <br /><br />
+        <div>
+          <button class="myButton" @click="newCoasts = !newCoasts">
+            <div v-if="!newCoasts">ADD NEW COST +</div>
+            <div v-else>cancel</div>
+          </button>
+          <br /><br /><br />
+          <div class="PaymentForm" v-show="newCoasts">
+            <PaymentForm :items="paymentsList" @add="onDataAdded" />
+          </div>
+        </div>
+        <div class="PaymentsList" v-show="!newCoasts">
+          <PaymentsList :items="paymentsList" />
+        </div>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+import PaymentsList from "./components/PaymentsList";
+import PaymentForm from "./components/PaymentForm";
+
+export default {
+  name: "App",
+  components: {
+    PaymentsList,
+    PaymentForm,
+  },
+  data() {
+    return {
+      paymentsList: [
+        {
+          date: "13.05.2021",
+          category: "Education",
+          price: 123,
+        },
+        {
+          date: "12.05.2021",
+          category: "Education",
+          price: 456,
+        },
+        {
+          date: "11.05.2021",
+          category: "Education",
+          price: 789,
+        },
+        {
+          date: "10.05.2021",
+          category: "Education",
+          price: 0,
+        },
+      ],
+      newCoasts: false,
+    };
+  },
+  methods: {
+    onDataAdded(data) {
+      this.paymentsList.push(data);
+      this.newCoasts = !this.newCoasts;
+    },
+  },
+};
+</script>
+
+<style lang="scss" module>
+.header {
+  color: red;
+  font-size: 40px;
+}
+.center {
+  margin: 50px calc(50% - 400px);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
+<style lang="scss">
+.myButton {
+  background-color: #1ea896;
+  color: white;
+  padding: 7px;
+  border: none;
+  border-radius: 5px;
+  width: 135px;
+}
+.myButton:hover {
+  background-color: calc(#1ea896 + #100);
+}
+.inputDate {
+  width: 95%;
+  height: 30px;
+  border: none;
+  text-align: center;
+  vertical-align: middle;
+  background-color: white;
+}
+.dateDate {
+  margin-top: -1px;
+}
+.select {
+  height: 34px;
+  width: 150px;
+}
+.price {
+  width: 100px;
+}
+.table {
+  width: 600px;
+  border: none;
+  border-collapse: collapse;
+  .cell {
+    border: solid 0.5px lightgray;
+    height: 30px;
+    text-align: center;
+    vertical-align: middle;
+  }
+}
+</style>
---
+++ src/components/PaymentForm.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+    <div>
+      <table class="table">
+        <tr>
+          <td class="cell"><b>№</b></td>
+          <td class="cell dateDate"><b>Date</b></td>
+          <td class="cell select"><b>Category</b></td>
+          <td class="cell price"><b>Price</b></td>
+        </tr>
+        <tr>
+          <td class="cell">{{ items.length + 1 }}</td>
+
+          <td class="cell dateDate">
+            <input
+              class="inputDate dateDate"
+              type="date"
+              placeholder="Date"
+              v-model="date"
+            />
+          </td>
+          <td class="cell select">
+            <!--  contenteditable="true" -->
+            <select
+              class="inputDate select"
+              placeholder="please choise category"
+              v-model="category"
+            >
+              <option
+                class="inputDate"
+                v-for="item in categoryList"
+                v-bind:key="item"
+              >
+                {{ item }}
+              </option>
+            </select>
+          </td>
+          <td class="cell price">
+            <input
+              class="inputDate price"
+              type="number"
+              placeholder="Price"
+              v-model.number="price"
+            />
+          </td>
+        </tr>
+      </table>
+    </div>
+    <br />
+    <br />
+    <button class="myButton" @click="save">ADD +</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      date: "",
+      categoryList: [
+        "Relaxation",
+        "Work",
+        "Education",
+        "Entertainment",
+        "Travel",
+      ],
+      category: "",
+      price: 0,
+    };
+  },
+  props: {
+    items: Array,
+  },
+  methods: {
+    save() {
+      const { date, category, price } = this;
+      this.$emit("add", { date, category, price });
+      this.date = 0;
+      this.category = 0;
+      this.price = 0;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
+<style lang="scss" module>
+</style>
